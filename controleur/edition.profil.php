1393cd1edb720cf9b635370fd04e4ad8b62bdca3
Romain <barnoud.r@gmail.com>
2017-11-24 14:31:57 +0100

    Ajout du dossier controleur

---
+++ controleur/edition.profil.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Le contrôleur :
+ * - définit le contenu des variables à afficher
+ * - identifie et appelle la vue
+ */
+
+/**
+ * Contrôleur de l'edition de profil
+ */
+
+// on inclut le fichier modèle contenant les appels à la BDD
+include('./modele/requetes.utilisateurs.php');
+
+
+
+?>
---
+++ controleur/pannes.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Le contrôleur :
+ * - définit le contenu des variables à afficher
+ * - identifie et appelle la vue
+ */
+
+/**
+ * Contrôleur des pannes
+ */
+
+// on inclut le fichier modèle contenant les appels à la BDD
+include('./modele/requetes.utilisateurs.php');
+
+// faire un switch avec comme cas : 
+
+?>
